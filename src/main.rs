0615e0e9a6c76cca7233566882dd3e65be3afdc5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-18 15:02:33 +0530

    Stage 2: Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Library
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -9,12 +10,33 @@ fn main() {
     // Listen for incoming connections and handle them
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+/// Handles the incoming connection stream by reading the incoming data,
+/// parsing it, and writing a response back to the stream.
+fn handle_connection(stream: &mut std::net::TcpStream) {
+    /// The size of the buffer to read incoming data
+    const BUFFER_SIZE: usize = 1024;
+
+    // Read the incoming data from the stream
+    let mut buffer = [0; BUFFER_SIZE];
+    stream.read(&mut buffer).unwrap();
+
+    // Print the incoming data
+    println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+    // Write a response back to the stream
+    let response = "+PONG\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+
+    // Flush the stream to ensure the response is sent
+    stream.flush().unwrap();
 }
