b8a427c998b8d3b73fc8ac49ff0c06e983ba4250
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-18 10:32:03 +0530

    Add a note about Redis' port

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 
 fn main() {
     // Create a TCPListener and bind it to the given address and port
+    // Note: 6379 is the default port that Redis uses (You may have to stop any running Redis instances)
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     // Listen for incoming connections and handle them
