e45f40f2c91ef20aabe200ff4f123dc860d09ac5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-18 19:43:38 +0530

    Use `tokio`

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,44 @@
 // Library
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::thread;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
+// ----
+// MAIN
+// ----
+
+#[tokio::main]
+async fn main() {
     // Run the server on the given address and port
-    run_server("127.0.0.1:6379").unwrap();
+    run_server("127.0.0.1:6379").await.unwrap();
 }
 
-/// Runs the TCP server on the given address and port
-fn run_server(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
+// ----------
+// TCP SERVER
+// ----------
+
+/// Runs the TCP server on the given address, listening for incoming connections.
+/// The server will handle each incoming connection in a separate thread.
+async fn run_server(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
     // Create a TCPListener and bind it to the given address and port
     // Note: 6379 is the default port that Redis uses (You may have to stop any running Redis instances)
-    let listener = TcpListener::bind(addr)?;
+    let listener = TcpListener::bind(addr).await?;
 
     // Listen for incoming connections and handle them
-    for stream in listener.incoming() {
-        // Unwrap the stream to get the TcpStream object and clone it to allow multiple connections
-        let mut s = stream?.try_clone()?;
-
-        // Set the stream to non-blocking mode to allow multiple connections to be handled concurrently
-        // s.set_nonblocking(true)?;
-
-        // Spawn a new thread for each incoming connection
-        thread::spawn(move || {
-            handle_connection(&mut s).expect("Failed to handle connection");
+    loop {
+        // Accept an incoming connection ...
+        let (mut stream, _) = listener.accept().await?;
+        // ... and spawn a new thread for each incoming connection
+        tokio::spawn(async move {
+            handle_connection(&mut stream).await.unwrap();
         });
     }
-
-    Ok(())
 }
 
 /// Handles the incoming connection stream by reading the incoming data,
 /// parsing it, and writing a response back to the stream.
-fn handle_connection(stream: &mut std::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+async fn handle_connection(
+    stream: &mut tokio::net::TcpStream,
+) -> Result<(), Box<dyn std::error::Error>> {
     /// The size of the buffer to read incoming data
     const BUFFER_SIZE: usize = 1024;
 
@@ -41,7 +46,7 @@ fn handle_connection(stream: &mut std::net::TcpStream) -> Result<(), Box<dyn std
     loop {
         // Read the incoming data from the stream
         let mut buffer = [0; BUFFER_SIZE];
-        let bytes_read = stream.read(&mut buffer)?;
+        let bytes_read = stream.read(&mut buffer).await?;
 
         // If no bytes were read, the client has closed the connection
         if bytes_read == 0 {
@@ -53,10 +58,10 @@ fn handle_connection(stream: &mut std::net::TcpStream) -> Result<(), Box<dyn std
 
         // Write a response back to the stream
         let response = b"+PONG\r\n";
-        stream.write_all(response)?;
+        stream.write_all(response).await?;
 
         // Flush the stream to ensure the response is sent
-        stream.flush()?;
+        stream.flush().await?;
     }
 
     Ok(())
