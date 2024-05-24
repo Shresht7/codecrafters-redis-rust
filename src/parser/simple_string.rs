12f356e9d3b91467c01352a98ee30899dc26a8d3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-24 20:46:26 +0530

    Refactor simple string parsing logic to handle input directly in the `simple_string::parse` function

--- src/parser/simple_string.rs
+++ src/parser/simple_string.rs
@@ -1,24 +1,69 @@
 // Library
 use super::{reader, RESPData};
 
+/// The first byte of a simple string
+const FIRST_BYTE: u8 = b'+';
+
 // -------------------
 // PARSE SIMPLE STRING
 // -------------------
 
 /// Parses a `SimpleString` from the given input data
+///
+/// A simple string is encoded as follows:
+/// - A prefix of `+` followed by the string data
+/// - CRLF terminator sequence
+///
+/// Example:
+/// ```sh
+/// +hello world\r\n => "hello world"
+/// ```
 pub fn parse(input: &[u8]) -> Result<(RESPData, &[u8]), Box<dyn std::error::Error>> {
+    // Check if the input starts with the plus `+` character
+    if input.first() != Some(&FIRST_BYTE) {
+        return Err(SimpleStringParserError::InvalidFirstByte(input[0]).into());
+    }
     // Create a reader to help extract information from the input byte slice
     let mut bytes = reader::read(input);
 
-    // Find the position of the CRLF sequence in the input
+    // Find the position of the CRLF sequence in the byte slice
     let (end_pos, rest_pos) = bytes.find_crlf()?;
 
-    // Extract the simple string from the input up to the CRLF sequence
-    let simple_string = bytes.to(end_pos).as_string()?;
+    // Extract the simple string from the first byte up to the CRLF sequence
+    let simple_string = bytes.slice(1, end_pos).as_string()?;
 
     // Return the parsed simple string and the remaining input
     Ok((RESPData::SimpleString(simple_string), &input[rest_pos..]))
 }
+
+// ------
+// ERRORS
+// ------
+
+/// Errors that can occur while parsing a simple string
+#[derive(Debug)]
+pub enum SimpleStringParserError {
+    /// The first byte of the input data is invalid and does not match `+`
+    InvalidFirstByte(u8),
+}
+
+// Implement the `Display` trait for the `SimpleStringParserError` type
+impl std::fmt::Display for SimpleStringParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            SimpleStringParserError::InvalidFirstByte(byte) => {
+                write!(
+                    f,
+                    "Invalid first byte: expected '+', found '{}'",
+                    *byte as char
+                )
+            }
+        }
+    }
+}
+
+// Implement the `Error` trait for the `SimpleStringParserError` type
+impl std::error::Error for SimpleStringParserError {}
 
 // -----
 // TESTS
@@ -35,7 +80,7 @@ mod tests {
 
     #[test]
     fn should_parse_simple_string() {
-        let input = b"hello world\r\n";
+        let input = b"+hello world\r\n";
         let expected = RESPData::SimpleString("hello world".to_string());
         match parse(input) {
             Ok((actual, _)) => assert_eq!(actual, expected),
@@ -44,14 +89,8 @@ mod tests {
     }
 
     #[test]
-    fn should_error_on_invalid_input() {
-        let input = b"hello world";
-        assert!(parse(input).is_err());
-    }
-
-    #[test]
-    fn should_parse_empty_input() {
-        let input = b"\r\n";
+    fn should_parse_empty_string() {
+        let input = b"+\r\n";
         let expected = RESPData::SimpleString("".to_string());
         match parse(input) {
             Ok((actual, _)) => assert_eq!(actual, expected),
@@ -61,7 +100,7 @@ mod tests {
 
     #[test]
     fn should_return_the_remaining_input() {
-        let input = b"hello world\r\nextra data";
+        let input = b"+hello world\r\nextra data";
         let expected = RESPData::SimpleString("hello world".to_string());
         match parse(input) {
             Ok((actual, remaining)) => {
@@ -70,5 +109,17 @@ mod tests {
             }
             Err(err) => show(err),
         }
+    }
+
+    #[test]
+    fn should_error_on_invalid_first_byte() {
+        let input = b"invalid\r\n";
+        assert!(parse(input).is_err());
+    }
+
+    #[test]
+    fn should_error_on_non_terminating_input() {
+        let input = b"+hello world";
+        assert!(parse(input).is_err());
     }
 }
