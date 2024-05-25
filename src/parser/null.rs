7702cc866fb874a08f393f223c304d3654fcd8ba
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-25 20:40:25 +0530

    Refactor Null Parser

--- src/parser/null.rs
+++ src/parser/null.rs
@@ -1,11 +1,15 @@
 // Library
-use super::{reader::CRLF, RESPData};
+use super::{errors::ParserError, reader, RESPData};
+
+// The first byte of a null value
+const FIRST_BYTE: u8 = b'_';
 
 // ----------
 // PARSE NULL
 // ----------
 
 /// Parses a `Null` value from the given input data.
+///
 /// A null value is represented by the underscore `_` character followed by a CRLF sequence.
 /// A null value is used to represent the absence of a value in RESP2.
 /// In RESP3, a null value is represented by the `Null` data type.
@@ -15,27 +19,57 @@ use super::{reader::CRLF, RESPData};
 /// _\r\n
 /// ```
 pub fn parse(input: &[u8]) -> Result<(RESPData, &[u8]), Box<dyn std::error::Error>> {
+    // Create a reader to help extract information from the input byte slice
+    let mut bytes = reader::read(input);
+
+    // Check if the input starts with the underscore `_` character
+    let first_byte = bytes.first()?;
+    if first_byte != FIRST_BYTE {
+        return Err(Box::new(ParserError::InvalidFirstByte(
+            first_byte, FIRST_BYTE,
+        )));
+    }
+
     // Find the position of the first CRLF sequence
-    let end_pos = CRLF.len() + 1;
-
-    // Check if the input is long enough to contain a null value
-    if input.len() < end_pos {
-        return Err("Invalid input. Insufficient data".into());
-    }
-
-    // Check if the first byte is an underscore
-    if input[0] != b'_' {
-        return Err("Invalid input. Expected underscore".into());
-    }
+    let (start_pos, rest_pos) = bytes.find_crlf()?;
 
     // Check if the second byte is a CRLF sequence
-    if &input[1..end_pos] != CRLF {
-        return Err("Invalid input. Expected CRLF".into());
+    if start_pos != 1 {
+        return Err(NullParserError::InvalidSecondByte(input[1]).into());
     }
 
     // Return the parsed null value and the remaining input
-    Ok((RESPData::Null, &input[end_pos..]))
+    Ok((RESPData::Null, &input[rest_pos..]))
 }
+
+// ------
+// ERRORS
+// ------
+
+/// Errors that can occur while parsing a null value
+#[derive(Debug)]
+pub enum NullParserError {
+    // The second byte does not start the CRLF sequence
+    InvalidSecondByte(u8),
+}
+
+// Implement the `Display` trait for `NullParserError` type
+impl std::fmt::Display for NullParserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            NullParserError::InvalidSecondByte(got) => {
+                write!(
+                    f,
+                    "Invalid second byte. Should begin CRLF sequence but got {}",
+                    *got as char
+                )
+            }
+        }
+    }
+}
+
+// Implement the `Error` trait for the `NullParserError` type
+impl std::error::Error for NullParserError {}
 
 // -----
 // TESTS
