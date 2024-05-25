f5c4ad1a93394fe0e1585b68da4113f81d7c4e2d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-26 01:45:47 +0530

    Rename file from data_types to types

--- src/parser/resp/mod.rs
+++ src/parser/resp/mod.rs
@@ -4,7 +4,6 @@ pub(crate) mod big_number;
 pub(crate) mod boolean;
 pub(crate) mod bulk_error;
 pub(crate) mod bulk_string;
-pub(crate) mod data_types;
 pub(crate) mod double;
 pub(crate) mod integer;
 pub(crate) mod null;
@@ -12,4 +11,5 @@ pub(crate) mod simple_error;
 pub(crate) mod simple_string;
 
 // Exports
-pub use data_types::Type;
+pub(crate) mod types;
+pub use types::Type;
