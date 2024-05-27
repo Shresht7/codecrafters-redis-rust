01393133583f4b88634997e504c04f9e3deefda8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-27 22:15:52 +0530

    Remove redundant first byte

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -4,5 +4,5 @@ use crate::parser::resp;
 /// Handles the PING command.
 /// The PING command simply returns a PONG response.
 pub fn command(_args: &[resp::Type]) -> resp::Type {
-    resp::Type::SimpleString("+PONG\r\n".into())
+    resp::Type::SimpleString("PONG\r\n".into())
 }
