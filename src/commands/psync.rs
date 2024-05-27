3d740b3a8ba89fc4341f3f43b84d48e8188b96d8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-27 23:51:11 +0530

    Receive handshake (2/2)

--- src/commands/psync.rs
+++ src/commands/psync.rs
@@ -7,6 +7,31 @@ use std::sync::{Arc, Mutex};
 /// The command takes two arguments: the replication ID and the replication offset.
 /// The replica will use the replication ID to identify the master server.
 /// The replica will use the replication offset to request new data from the master server.
-pub fn command(_args: &[resp::Type], _server: &Arc<Mutex<Server>>) -> resp::Type {
-    resp::Type::SimpleString("FULLRESYNC 0123456789 123\r\n".into())
+pub fn command(args: &[resp::Type], server: &Arc<Mutex<Server>>) -> resp::Type {
+    // Check the number of arguments
+    if args.len() < 2 {
+        return resp::Type::SimpleError("ERR wrong number of arguments for 'PSYNC' command".into());
+    }
+
+    // Get the replication ID and offset from the arguments
+    // let repl_id = match &args[0] {
+    //     resp::Type::BulkString(id) => id.clone(),
+    //     _ => return resp::Type::SimpleError("ERR invalid replication ID".into()),
+    // };
+    // let repl_offset = match &args[1] {
+    //     resp::Type::BulkString(offset) => match offset.parse::<i32>() {
+    //         Ok(offset) => offset,
+    //         Err(_) => return resp::Type::SimpleError("ERR invalid replication offset".into()),
+    //     },
+    //     _ => return resp::Type::SimpleError("ERR invalid replication offset".into()),
+    // };
+
+    // FULLRESYNC
+    // if repl_id == "?" && repl_offset == -1 {
+    let repl_id = server.lock().unwrap().master_replid.clone();
+    let repl_offset = server.lock().unwrap().master_repl_offset;
+
+    // Send a full synchronization request to the master server
+    resp::Type::SimpleString(format!("FULLRESYNC {} {}", repl_id, repl_offset))
+    // }
 }
