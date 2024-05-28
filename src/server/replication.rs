f82b5a14982ba0e074a4517732a3b7d299d16ad4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-29 00:10:02 +0530

    Refactor replication handshake and connection setup

--- src/server/replication.rs
+++ src/server/replication.rs
@@ -45,7 +45,19 @@ impl Role {
 
 impl Role {
     /// Sends a handshake to the replication master server at the given address.
-    /// The handshake includes a PING command, REPLCONF listening-port, and REPLCONF capa psync2.
+    /// The handshake is used to synchronize the replica server with the master server.
+    /// The master server sends replication data to the replica server after the handshake is successful.
+    /// The handshake consists of the following steps:
+    /// 1. Send a PING command to the master server.
+    /// 2. Send a REPLCONF listening-port <PORT> command to inform the master server of the port that the replica server is listening on.
+    /// 3. Send a REPLCONF capa psync2 command to inform the master server that the replica server supports the PSYNC2 command.
+    /// 4. Send a PSYNC <REPLID> <OFFSET> command to synchronize the replica server with the master server.
+    /// The REPLID and OFFSET are used to identify the replication stream and the offset of the last received command.
+    /// The REPLID is "?" if the replica server is syncing for the first time.
+    /// The OFFSET is -1 if the replica server is syncing for the first time.
+    /// The REPLID and OFFSET are used to resume replication from the last received command.
+    /// Returns a connection to the master server if the handshake is successful.
+    /// This connection is used to receive replication data from the master server.
     pub async fn send_handshake(
         &self,
         port: u16,
@@ -62,37 +74,90 @@ impl Role {
         let mut connection = conn::new(stream);
 
         // Send a PING
-        let response = array(vec![bulk_string("PING")]);
-        connection.write_all(&response.as_bytes()).await?;
-        connection.read().await?; // Read the PONG response (not used)
+        send_ping(&mut connection).await?;
 
         // Send REPLCONF listening-port <PORT>
-        let response = array(vec![
-            bulk_string("REPLCONF"),
-            bulk_string("listening-port"),
-            bulk_string(port.to_string().as_str()),
-        ]);
-        connection.write_all(&response.as_bytes()).await?;
-        connection.read().await?; // Read the OK response (not used)
+        send_replconf_listening_port(&mut connection, port).await?;
 
         // Send REPLCONF capa psync2
-        let response = array(vec![
-            bulk_string("REPLCONF"),
-            bulk_string("capa"),
-            bulk_string("psync2"),
-        ]);
-        connection.write_all(&response.as_bytes()).await?;
-        connection.read().await?; // Read the OK response (not used)
+        send_replconf_capa_psync2(&mut connection).await?;
 
         // Send PSYNC <REPLID> <OFFSET>
-        let response = array(vec![
-            bulk_string("PSYNC"),
-            bulk_string("?"),
-            bulk_string("-1"),
-        ]);
-        connection.write_all(&response.as_bytes()).await?;
-        connection.read().await?; // Read the FULLRESYNC response (not used)
+        psync(&mut connection, "?", -1).await?;
 
+        // Return the connection to the master server so that
+        // we can re-use the same connection for replication.
         Ok(connection)
     }
+}
+
+// PING
+// ----
+
+/// Sends a PING command to the replication master server.
+async fn send_ping(connection: &mut conn::Connection) -> Result<(), Box<dyn std::error::Error>> {
+    // Send a PING
+    let response = array(vec![bulk_string("PING")]);
+    connection.write_all(&response.as_bytes()).await?;
+    connection.read().await?; // Read the PONG response (not used)
+    Ok(())
+}
+
+// REPLCONF listening-port <PORT>
+// ------------------------------
+
+/// Sends a REPLCONF listening-port <PORT> command to the replication master server.
+/// The command is used to inform the master server of the port that the replica server is listening on.
+/// The master server uses this information to send replication data to the replica server.
+async fn send_replconf_listening_port(
+    connection: &mut conn::Connection,
+    port: u16,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Send REPLCONF listening-port <PORT>
+    let response = array(vec![
+        bulk_string("REPLCONF"),
+        bulk_string("listening-port"),
+        bulk_string(port.to_string().as_str()),
+    ]);
+    connection.write_all(&response.as_bytes()).await?;
+    connection.read().await?; // Read the OK response (not used)
+    Ok(())
+}
+
+// REPLCONF capa psync2
+// --------------------
+
+// REPLCONF capa psync2 is sent as part of the handshake to inform the master server that the replica server supports the PSYNC2 command.
+async fn send_replconf_capa_psync2(
+    connection: &mut conn::Connection,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Send REPLCONF capa psync2
+    let response = array(vec![
+        bulk_string("REPLCONF"),
+        bulk_string("capa"),
+        bulk_string("psync2"),
+    ]);
+    connection.write_all(&response.as_bytes()).await?;
+    connection.read().await?; // Read the OK response (not used)
+    Ok(())
+}
+
+// PSYNC
+// -----
+
+// PSYNC is used to synchronize the replica server with the master server.
+async fn psync(
+    connection: &mut conn::Connection,
+    replid: &str,
+    offset: i64,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Send PSYNC <REPLID> <OFFSET>
+    let response = array(vec![
+        bulk_string("PSYNC"),
+        bulk_string(replid),
+        bulk_string(offset.to_string().as_str()),
+    ]);
+    connection.write_all(&response.as_bytes()).await?;
+    connection.read().await?; // Read the FULLRESYNC response (not used)
+    Ok(())
 }
