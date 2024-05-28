2ed489fb62d95f2fb2ba26a10f856a38bf6fb9a9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-28 23:56:41 +0530

    Create helper function to split host and port from an address

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -18,6 +18,23 @@ pub fn generate_id(len: u16) -> String {
     id
 }
 
+/// Split the host and port from the given address.
+/// The address should be in the format "host:port".
+///
+/// ```rs
+/// let addr = "127.0.0.1:6379";
+/// let (host, port) = split_host_and_port(addr).unwrap();
+/// assert_eq!(host, "127.0.0.1"); // true
+/// assert_eq!(port, 6379); // true
+/// ```
+pub fn split_host_and_port(addr: &str) -> Result<(&str, u16), Box<dyn std::error::Error>> {
+    let parts: Vec<&str> = addr.split(':').collect();
+    let host = parts.get(0).ok_or("Invalid address")?;
+    let port = parts.get(1).ok_or("Invalid address")?;
+    let port = port.parse::<u16>()?;
+    Ok((host, port))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +52,27 @@ mod tests {
         assert_eq!(id1.len(), 40);
         let id2 = generate_id(24);
         assert_eq!(id2.len(), 24);
+    }
+
+    #[test]
+    fn should_split_host_and_port() {
+        let addr = "127.0.0.1:6379";
+        let (host, port) = split_host_and_port(addr).unwrap();
+        assert_eq!(host, "127.0.0.1");
+        assert_eq!(port, 6379);
+    }
+
+    #[test]
+    fn should_fail_to_split_host_and_port() {
+        let addr = "127.0.0.1";
+        let result = split_host_and_port(addr);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn should_fail_to_split_host_and_port_with_invalid_port() {
+        let addr = "127.0.0.1:invalid";
+        let result = split_host_and_port(addr);
+        assert!(result.is_err());
     }
 }
