3619dfd15508f30c231fbc77c2f1dc4661bd4c12
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-28 21:33:05 +0530

    Refactor Config Parsing Logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Modules
-mod cli;
 mod commands;
+mod config;
 mod database;
 mod helpers;
 mod parser;
@@ -12,22 +12,18 @@ mod server;
 
 #[tokio::main]
 async fn main() {
-    // Parse the command-line arguments
+    // Parse the configuration parameters from the command-line arguments
     let args: Vec<String> = std::env::args().collect();
-    let mut cli = cli::CommandLineArguments::default();
-    if let Err(e) = cli.parse(args) {
-        eprintln!("Failed to parse the command line arguments!\n{}", e);
-        return;
-    }
+    let config = config::from_command_line(args).expect("Failed to parse command-line arguments");
 
     // Determine the address using the port variable
-    let port = cli.port; // Default port is 6379
+    let port = config.port; // Default port is 6379
 
     // Instantiate the server with the address
     let mut server = server::new("127.0.0.1", port);
 
     // If the replica-of address is set, the server will act as a replica
-    if let Some(replicaof) = cli.replicaof {
+    if let Some(replicaof) = config.replicaof {
         println!("Replicating from: {}", replicaof);
         server.replicaof(&replicaof);
     }
