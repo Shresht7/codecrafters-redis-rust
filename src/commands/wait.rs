6312814cbc6756bdd88c651ffe8b468dc0b5e8ab
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-29 19:12:38 +0530

    Parse arguments in the WAIT command

--- src/commands/wait.rs
+++ src/commands/wait.rs
@@ -6,14 +6,33 @@ use crate::{
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+/// Handle the WAIT command.
+/// The WAIT command blocks the client until the specified number of replicas for the specified key is reached,
+/// or the timeout is reached. The command is used to wait for the completion of a write operation on a replica.
 pub async fn command(
-    _args: &[resp::Type],
+    args: &[resp::Type],
     connection: &mut Connection,
-    server: &Arc<Mutex<Server>>,
+    _server: &Arc<Mutex<Server>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let server = server.lock().await;
-    let replicas = server.replicas.clone();
-    let response = resp::Type::Integer(replicas.len() as i64);
-    connection.write_all(&response.as_bytes()).await?;
+    // Check if the number of arguments is correct
+    if args.len() < 2 {
+        let response =
+            resp::Type::SimpleError("ERR wrong number of arguments for 'wait' command".to_string());
+        connection.write_all(&response.as_bytes()).await?;
+        return Ok(());
+    }
+
+    // Extract number of replicas and timeout from the arguments
+    let replicas = match &args[0] {
+        resp::Type::BulkString(replicas) => Some(replicas.parse::<u32>()?),
+        _ => None,
+    };
+    let timeout = match &args[1] {
+        resp::Type::BulkString(timeout) => Some(timeout.parse::<u32>()?),
+        _ => None,
+    };
+
+    println!("WAIT replicas: {:?}, timeout: {:?}", replicas, timeout);
+
     Ok(())
 }
