72b0c96a675ac452154fdd8dd0151819f66e9561
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-31 03:19:50 +0530

    Refactor WAIT command

--- src/commands/wait.rs
+++ src/commands/wait.rs
@@ -9,6 +9,10 @@ use tokio::{
     time::Instant,
 };
 
+// ----
+// WAIT
+// ----
+
 /// Handle the WAIT command.
 /// The WAIT command blocks the client until the specified number of replicas for the specified key is reached,
 /// or the timeout is reached. The command is used to wait for the completion of a write operation on a replica.
@@ -18,10 +22,9 @@ pub async fn command(
     server: &Arc<Mutex<Server>>,
     wait_channel: &Arc<Mutex<(mpsc::Sender<u64>, mpsc::Receiver<u64>)>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    // Get the role, master_repl_offset, and number of replicas from the server
     let (role, master_repl_offset, addresses) = {
-        println!("wait locking server ...");
         let server = server.lock().await;
-        print!("locked 🔒");
         (
             server.role.clone(),
             server.master_repl_offset.clone(),
@@ -31,122 +34,122 @@ pub async fn command(
 
     // Check if the server is a master
     if !role.is_master() {
-        let response = resp::Type::SimpleError("ERR This instance is not a master. WAIT command is only available on the master instance.".to_string());
-        connection.write_all(&response.as_bytes()).await?;
-        return Ok(());
+        return connection
+            .write_error("ERR WAIT command is only valid on master")
+            .await;
     }
 
     // Check if the number of arguments is correct
     if args.len() < 2 {
-        let response =
-            resp::Type::SimpleError("ERR wrong number of arguments for 'wait' command".to_string());
-        connection.write_all(&response.as_bytes()).await?;
-        return Ok(());
+        return connection
+            .write_error("ERR wrong number of arguments for 'WAIT' command. Usage: WAIT <num_replicas> <timeout>")
+            .await;
     }
 
-    // Extract number of replicas and timeout from the arguments
+    // Extract the desired number of replicas from the arguments
     let desired_replicas = match &args[0] {
-        resp::Type::BulkString(replicas) => replicas.parse::<u32>()?,
-        _ => {
-            let response = resp::Type::SimpleError("ERR invalid number of replicas".to_string());
-            connection.write_all(&response.as_bytes()).await?;
-            return Ok(());
+        resp::Type::BulkString(replicas) => replicas.parse::<usize>()?,
+        x => {
+            return connection
+                .write_error(format!("ERR invalid number of replicas {:?}", x))
+                .await;
         }
     };
-    let desired_replicas = if desired_replicas as usize > addresses {
+    // If the desired number of replicas is greater than the number of replicas, set the desired number of replicas to the number of replicas
+    let desired_replicas = if desired_replicas > addresses {
         addresses
     } else {
         desired_replicas as usize
     };
+
+    // Extract the timeout from the arguments
     let timeout = match &args[1] {
-        resp::Type::BulkString(timeout) => timeout.parse::<u32>()?,
-        _ => {
-            let response = resp::Type::SimpleError("ERR invalid timeout".to_string());
-            connection.write_all(&response.as_bytes()).await?;
-            return Ok(());
+        resp::Type::BulkString(timeout) => timeout.parse::<u64>()?,
+        x => {
+            return connection
+                .write_error(format!("ERR invalid timeout {:?}", x))
+                .await;
         }
     };
-    let timeout = Instant::now() + Duration::from_millis(timeout as u64);
+    // Calculate the timeout
+    let timeout = Instant::now() + Duration::from_millis(timeout);
 
-    // Counter to keep track of the number of replicas that have been synced
     let mut synced_replicas = 0;
 
+    // Counter to keep track of the number of bytes to send later
     let mut later_bytes = 0;
 
-    // If the master_repl_offset is 0, return the number of replicas
-    if master_repl_offset == 0 {
-        let response = resp::Type::Integer(addresses as i64);
-        connection.write_all(&response.as_bytes()).await?;
-        return Ok(());
-    }
-
-    // Flag to indicate if this is the first iteration
-    let mut first_iteration = true;
-    while Instant::now() < timeout {
-        // If the number of synced replicas reaches the desired number, break the loop
-        if synced_replicas >= desired_replicas {
-            println!(
-                "Number of synced replicas reached the desired number: {}/{}",
-                synced_replicas, desired_replicas
-            );
-            break;
-        }
-
-        // If this is the first iteration, send the REPLCONF GETACK command
-        if first_iteration {
-            let command = Type::Array(vec![
-                Type::BulkString("REPLCONF".to_string()),
-                Type::BulkString("GETACK".to_string()),
-                Type::BulkString("*".to_string()),
-            ]);
-            later_bytes += command.as_bytes().len();
-            println!("Sending REPLCONF GETACK * command");
-            {
-                println!("wait locking ...");
-                let s = server.lock().await;
-                print!("locked 🔒");
-                s.sender.send(command)?;
+    // Counter to keep track of the number of replicas that have been synced
+    synced_replicas = if master_repl_offset == 0 {
+        // If the master_repl_offset is 0, return the number of replicas
+        addresses
+    } else {
+        // Flag to indicate if this is the first iteration
+        let mut first_iteration = true;
+        // Loop until the timeout is reached or the number of synced replicas reaches the desired number
+        while Instant::now() < timeout {
+            // If the number of synced replicas reaches the desired number, break the loop
+            if synced_replicas >= desired_replicas {
+                println!(
+                    "Number of synced replicas reached the desired number: {}/{}",
+                    synced_replicas, desired_replicas
+                );
+                break;
             }
-        }
-        first_iteration = false; // Set the flag to false after the first iteration to avoid sending the REPLCONF GETACK command indefinitely
 
-        // Sleep for 20 milliseconds
-        tokio::time::sleep(Duration::from_millis(50)).await;
+            // If this is the first iteration, send the REPLCONF GETACK command
+            if first_iteration {
+                let command = Type::Array(vec![
+                    Type::BulkString("REPLCONF".to_string()),
+                    Type::BulkString("GETACK".to_string()),
+                    Type::BulkString("*".to_string()),
+                ]);
+                later_bytes += command.as_bytes().len();
+                println!("Sending REPLCONF GETACK * command");
+                {
+                    let s = server.lock().await;
+                    s.sender.send(command)?;
+                }
+            }
+            first_iteration = false; // Set the flag to false after the first iteration to avoid sending the REPLCONF GETACK command indefinitely
 
-        {
-            // Await response from the replica
-            println!("wait locking ...");
-            let mut wc = wait_channel.lock().await;
-            println!("locked 🔒");
-            loop {
-                match wc.1.try_recv() {
-                    Ok(offset) => {
-                        println!(
-                            "Received offset from replica: {}, master repl offset is {}",
-                            offset, master_repl_offset
-                        );
-                        // If the offset is greater than or equal to the master_repl_offset, increment the synced_replicas counter
-                        if offset >= master_repl_offset {
-                            println!("Replica is synced");
-                            synced_replicas += 1;
-                        }
-                        // If the number of synced replicas reaches the desired number, break the loop
-                        if synced_replicas >= desired_replicas {
+            // Sleep for a few milliseconds
+            tokio::time::sleep(Duration::from_millis(50)).await;
+
+            {
+                // Await response from the replica
+                let mut wc = wait_channel.lock().await;
+                loop {
+                    match wc.1.try_recv() {
+                        Ok(offset) => {
                             println!(
-                                "Number of synced replicas reached the desired number: {}/{}",
-                                synced_replicas, desired_replicas
+                                "Received offset from replica: {}, master repl offset is {}",
+                                offset, master_repl_offset
                             );
+                            // If the offset is greater than or equal to the master_repl_offset, increment the synced_replicas counter
+                            if offset >= master_repl_offset {
+                                println!("Replica is synced");
+                                synced_replicas += 1;
+                            }
+                            // If the number of synced replicas reaches the desired number, break the loop
+                            if synced_replicas >= desired_replicas {
+                                println!(
+                                    "Number of synced replicas reached the desired number: {}/{}",
+                                    synced_replicas, desired_replicas
+                                );
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("No response from replica. Error: {:?}", e);
                             break;
                         }
-                    }
-                    Err(e) => {
-                        eprintln!("No response from replica. Error: {:?}", e);
-                        break;
                     }
                 }
             }
         }
-    }
+        synced_replicas
+    };
 
     println!("Number of synced replicas: {}", synced_replicas);
 
@@ -156,9 +159,7 @@ pub async fn command(
 
     // Add the bytes that were sent later to the master_repl_offset
     {
-        println!("wait locking ...");
         let mut s = server.lock().await;
-        print!("locked 🔒");
         s.master_repl_offset += later_bytes as u64;
     }
 
