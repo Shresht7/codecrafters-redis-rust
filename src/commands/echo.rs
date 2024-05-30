536d035ee557b11081a8c0a577705c06e7bd63d0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-31 02:35:46 +0530

    Refactor ECHO command

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -1,6 +1,10 @@
 // Library
 use crate::{parser::resp::Type, server::connection::Connection};
 
+// ----
+// ECHO
+// ----
+
 /// Handles the ECHO command.
 /// The ECHO command simply returns the argument provided to it.
 pub async fn command(
@@ -9,19 +13,17 @@ pub async fn command(
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Check the number of arguments
     if args.len() < 1 {
-        let response =
-            Type::SimpleError("ERR at least one argument is required for 'ECHO' command".into());
-        connection.write_all(&response.as_bytes()).await?;
-        return Ok(());
+        return connection
+            .write_error("ERR wrong number of arguments for 'ECHO' command. Usage ECHO message")
+            .await;
     }
 
     // Respond with the argument
-    let response = if let Some(Type::BulkString(arg)) = args.get(0) {
-        Type::BulkString(arg.clone())
-    } else {
-        Type::SimpleError("ERR invalid argument type for 'ECHO' command".into())
+    let response = match args.get(0) {
+        Some(Type::BulkString(arg)) => Type::BulkString(arg.clone()),
+        _ => Type::SimpleError("ERR invalid argument type for 'ECHO' command".into()),
     };
-
     connection.write_all(&response.as_bytes()).await?;
+
     Ok(())
 }
